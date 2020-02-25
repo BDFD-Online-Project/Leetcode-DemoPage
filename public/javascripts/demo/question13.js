e26150dc38b1cdc81785336023385541f8ea9e86
Kelvin Chen <bdfd2005@gmail.com>
2020-02-24 20:27:39 -0500

    1

--- public/javascripts/demo/question13.js
+++ public/javascripts/demo/question13.js
@@ -1,6 +1,6 @@
 document.title = 'Question 13 Roman to Integer Demo Page';
 var Roman_to_Number = function(num) {
-  var result= "";
+  var result = '';
   var RomanToNumber = {
     M: 1000,
     CM: 900,
@@ -15,11 +15,11 @@ var Roman_to_Number = function(num) {
     V: 5,
     IV: 4,
     I: 1
-  }
-  for (var key in RomanToNumber){
-    if(num >= RomanToNumber[key]){
-      result += key.repeat(Math.trunc(num/RomanToNumber[key]));
-      num -= RomanToNumber[key]*Math.trunc(num/RomanToNumber[key]);
+  };
+  for (var key in RomanToNumber) {
+    if (num >= RomanToNumber[key]) {
+      result += key.repeat(Math.trunc(num / RomanToNumber[key]));
+      num -= RomanToNumber[key] * Math.trunc(num / RomanToNumber[key]);
     }
   }
   return result;
@@ -27,27 +27,34 @@ var Roman_to_Number = function(num) {
 
 // console.log(Reverse_Integer(123));
 window.onload = function() {
-  submit.addEventListener("click", function(){
-      var input = document.getElementById("input");
-      var submit = document.getElementById("submit");
-      var demo1 = document.getElementById("demo1");
-      var demo2 = document.getElementById("demo2");
-      var iv = input.value;
-    if(iv === "") return   alert('this is not valid');
-    //* Important Notice when input from HTML is string 
-    if(iv === "0") return   alert('reverse 0 is 0, please input other number to verify'); 
-    if(parseInt(iv) >= 3999 ) return   alert('the number is out of positive range, and result will be define as out of range！'); 
-    if(parseInt(iv) <= 0 ) return   alert('the number is out of negative range, and result will be define as out of range！'); 
+  submit.addEventListener('click', function() {
+    var input = document.getElementById('input');
+    var submit = document.getElementById('submit');
+    var demo1 = document.getElementById('demo1');
+    var demo2 = document.getElementById('demo2');
+    var iv = input.value;
+    if (iv === '') return alert('this is not valid');
+    //* Important Notice when input from HTML is string
+    if (iv === '0')
+      return alert('reverse 0 is 0, please input other number to verify');
+    if (parseInt(iv) >= 3999)
+      return alert(
+        'the number is out of positive range, and result will be define as out of range！'
+      );
+    if (parseInt(iv) <= 0)
+      return alert(
+        'the number is out of negative range, and result will be define as out of range！'
+      );
     // console.log(iv);&& "
-    // console.log(typeof iv); 
+    // console.log(typeof iv);
     // console.log(Reverse_Integer(iv));
-    submit.onclick = function (){
-      var oiv = (iv);
-      var ov= Roman_to_Number(parseInt(iv));
+    submit.onclick = function() {
+      var oiv = iv;
+      var ov = Roman_to_Number(parseInt(iv));
       // console.log(typeof oiv);
       // console.log(typeof ov);
       demo1.innerHTML = oiv;
       demo2.innerHTML = ov;
-    }
-  })
-}
+    };
+  });
+};
